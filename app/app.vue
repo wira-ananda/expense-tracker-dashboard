2a3690d77d0b712b6d000d2c6a4e456e09d777c9
Wira Ananda <ur.wiraananda@gmail.com>
2026-03-24 02:38:54 +0800

    fix: memperbaiki kesalahan sintaks di default layout utk konsistensi lint

--- app/app.vue
+++ app/app.vue
@@ -12,8 +12,8 @@ useHead({
 })
 
 const title = 'Nuxt Starter Template'
-const description =
-  'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const description
+  = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
 
 useSeoMeta({
   title,
