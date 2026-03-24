be2f1c092d16f3e0462a38e80d0edac6b74b7786
Wira Ananda <ur.wiraananda@gmail.com>
2026-03-24 17:38:58 +0800

    fix: perbarui syntax eslint.config.mjs matikan aturan gaya

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,8 +7,8 @@ export default withNuxt({
     '@stylistic/operator-linebreak': 'off',
     // Mematikan komplain soal spasi/indentasi yang sering berantem sama Prettier
     '@stylistic/indent': 'off',
-    '@stylistic/member-delimiter-style': 'off'
+    '@stylistic/member-delimiter-style': 'off',
     // Tambahkan ini juga agar 'any' tidak error lagi
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 })
