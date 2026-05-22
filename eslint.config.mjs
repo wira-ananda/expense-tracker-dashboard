71e82c6e6554c7c8c41d22c0a3bac6de4b3b4f20
Wira Ananda <wiraananda@MacBook-Air-Wira.local>
2026-05-22 11:48:07 +0800

    chore(lint): relax Vue formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
+export default withNuxt().append({
   rules: {
-    // Membolehkan '=' di akhir atau awal baris (bebas)
     '@stylistic/operator-linebreak': 'off',
-    // Mematikan komplain soal spasi/indentasi yang sering berantem sama Prettier
     '@stylistic/indent': 'off',
     '@stylistic/member-delimiter-style': 'off',
-    // Tambahkan ini juga agar 'any' tidak error lagi
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@stylistic/arrow-parens': 'off',
+    '@stylistic/indent-binary-ops': 'off',
+
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': 'off'
   }
 })
